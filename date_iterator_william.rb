8d79c27166bdc8626e8dcdcacdc3a9466ccf69b9
WillyRuz <44385063+WillyRuz@users.noreply.github.com>
2019-11-12 17:53:02 -0600

    Update date_iterator_william.rb

--- date_iterator_william.rb
+++ date_iterator_william.rb
@@ -1,15 +1,16 @@
 require 'date'
+
 def date_iterator(first_date, second_date, day_number)  
   first_date_change = Date.parse(first_date)
   second_date_change = Date.parse(second_date)
-  arr=[]
+  arr = []
   month = first_date_change.mon
   year = first_date_change.year
-  month_en = second_date_change.mon
-  while month <= month_en do 
+  month_end = second_date_change.mon
+  while month <= month_end do 
     arr.push(Date.new(year, month, day_number).to_s)
-    month+=1
+    month += 1
   end
-  return arr
+  arr
 end  
 date_iterator('2018-01-02', '2018-05-05', 4)
