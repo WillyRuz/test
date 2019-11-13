49d41487f343ecf2619f425d8904e3ccb09b3fad
MemoHomie <guillermodelrio10@gmail.com>
2019-11-13 10:18:35 -0600

    agregado de rama master

---
+++ date_iterator_william.rb
@@ -0,0 +1,16 @@
+require 'date'
+
+def date_iterator(first_date, second_date, day_number)  
+  first_date_change = Date.parse(first_date)
+  second_date_change = Date.parse(second_date)
+  arr=[]
+  month = first_date_change.mon
+  year = first_date_change.year
+  month_en = second_date_change.mon
+  while month <= month_en do 
+    arr.push(Date.new(year, month, day_number).to_s)
+    month+=1
+  end
+  return arr
+end  
+date_iterator('2018-01-02', '2018-05-05', 4)
