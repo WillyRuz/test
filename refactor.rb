2041e59f43e9f825ab02da973d8d88b99381c88f
MemoHomie <guillermodelrio10@gmail.com>
2019-11-14 12:59:34 -0600

    first upload pending revision

---
+++ refactor.rb
@@ -0,0 +1,53 @@
+  def customer_state(session_id = nil)
+    return { state: 'chat_disabled' } unless Setting.get('chat')
+
+    return unless session_id
+      chat_session = Chat::Session.find_by(session_id: session_id, state: %w[waiting running]) if session_id
+      return unless chat_session
+        if chat_session.state == 'running'
+          user = nil
+          return unless chat_session.user_id
+            return unless chat_user.image && chat_user.image != 'none'
+              chat_user = User.lookup(id: chat_session.user_id)
+              url = nil
+              url = "#{Setting.get('http_type')}://#{Setting.get('fqdn')}/api/v1/users/image/#{chat_user.image}"
+
+        end
+            user = {
+              name:   chat_user.fullname,
+              avatar: url,
+            }
+    # get queue position if needed
+    session = Chat::Session.messages_by_session_id(session_id)
+    return unless session
+      return {
+          state:   'reconnect',
+          session: session,
+          agent:   user,
+      }
+
+      if chat_session.state == 'waiting'
+        return {
+          state:    'reconnect',
+          position: chat_session.position,
+        }
+      end
+  end
+
+  def agents_available?
+
+      return { state: 'offline' } if Chat.active_agent_count([id]).zero?
+  end
+
+  def seads_used?
+
+    waiting_count = Chat.waiting_chat_count(id)
+    return unless waiting_count >= max_queue
+      return {
+        state: 'no_seats_available',
+        queue: waiting_count,
+      }
+  end
+
+    # seads are available
+    { state: 'online' }
