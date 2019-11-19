69cac74c6ab66a722a2d619d2fa6c49bfaf60367
MemoHomie <guillermodelrio10@gmail.com>
2019-11-19 11:59:17 -0600

    first commit for revision

---
+++ ejercicio_clase.rb
@@ -0,0 +1,15 @@
+class User
+  attr_accessor :name, :last_name
+
+  def initialize(name, last_name)
+    @name = name
+    @last_name = last_name
+  end
+
+  def full_name
+    "#{@name}  #{@last_name}"
+  end
+end
+
+user = User.new('William', 'Ruz')
+puts user.full_name
