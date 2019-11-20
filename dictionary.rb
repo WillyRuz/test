4b48a3a8dc7122a70d9cf6ea8eebf7e822951b7b
MemoHomie <guillermodelrio10@gmail.com>
2019-11-20 13:15:00 -0600

    dictionary- second commit corrected

--- dictionary.rb
+++ dictionary.rb
@@ -2,16 +2,26 @@ class Dictionary
   attr_accessor :language, :word
 
   def translate(language, word)
-    words_en = { department: 'departamento', tenant: 'inquilino', owner: 'propietario', rent: 'renta' }
-    words_es = { departamento: 'apartement', inquilino: ' tenant', propietario: 'owner', renta: 'rent' }
+    words_en = {
+      department: 'departamento',
+      tenant: 'inquilino',
+      owner: 'propietario',
+      rent: 'renta'
+    }
+    words_es = {
+      departamento: 'apartement',
+      inquilino: ' tenant',
+      propietario: 'owner',
+      renta: 'rent'
+    }
 
-    unless language.downcase == 'español'
-      words_es.fetch(word.downcase)
+    if language.downcase != 'español'
+      return puts words_es.fetch(word.downcase)
     end
 
-    words_en.fetch(word.downcase)
+    puts words_en.fetch(word.downcase)
   end
 end
 
 new = Dictionary.new
-new.translate('español', :rent)
+new.translate('ENGLISH', :renta)
